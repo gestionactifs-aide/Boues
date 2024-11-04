461917424bebe9778bd4e493447072040f380546
gestionactifs-aide <gestionactifs.aide@gmail.com>
2024-11-04 08:09:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App() {
       </div>
       <header className="App-header">
         <div className="Logo-container">
-          <img src="/AIDE_10mm.png" alt="Logo AIDE" className="App-logo" />
+          <img src={`${process.env.PUBLIC_URL}/AIDE_10mm.png`} alt="Logo AIDE" className="App-logo" />
         </div>
-		<div className="Title-container">
-		  <h1 className="App-title">
-			Association intercommunale pour le démergement <br /> et l'épuration des communes de la province de Liège
-		  </h1>
-		</div>
+        <div className="Title-container">
+          <h1 className="App-title">
+            Association intercommunale pour le démergement <br /> et l'épuration des communes de la province de Liège
+          </h1>
+        </div>
       </header>
       <div className="Image-container">
-        <img src="/SE_Sclessin.png" alt="SE Sclessin" className="Map-image" />
+        <img src={`${process.env.PUBLIC_URL}/SE_Sclessin.png`} alt="SE Sclessin" className="Map-image" />
       </div>
-	  <div className="Project-container">
-      <h2 className="Project-title">Stations d'épuration - Transport de boues</h2>
+      <div className="Project-container">
+        <h2 className="Project-title">Stations d'épuration - Transport de boues</h2>
       </div>
       <div className="Map-container">
         <MapComponent />
@@ -34,5 +34,7 @@ function App() {
     </div>
   );
 }
+
+export default App;
 
 export default App;
